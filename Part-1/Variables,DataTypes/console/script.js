fc0f1c725e5e2d968d3be79cf2c1607486a71d4b
Divyanshu Jain <divyanshujain111@gmail.com>
2023-09-20 14:42:27 +0530

    Learned about different types of consoles

---
+++ Part-1/Variables,DataTypes/console/script.js
@@ -0,0 +1,15 @@
+console.log(100);
+console.log('Hello World');
+console.log(20,'Hello',true);
+let x = 100;
+console.log(x);
+console.error('Error');
+console.warn('warning');
+console.table({name:'Divyanshu',email:'divyanshujain121@gmail.com'});
+console.group('simple');
+console.log(x);
+console.error('Error');
+console.warn('Warning');
+console.groupEnd();
+const styles = 'padding:10px;background-color:white;color:green';
+console.log('%c Styles are applied ',styles);
